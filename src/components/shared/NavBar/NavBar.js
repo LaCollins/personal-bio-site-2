76993da34ed7adeb189bb19bd99710831313ee8e
Laura Collins <laura.e.collins1@gmail.com>
2020-03-24 14:54:17 -0500

    navbar

---
+++ src/components/shared/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import './NavBar.scss';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+class NavBar extends React.Component {
+  render() {
+    return (
+      <div className="NavBar">
+        <nav className="container-fluid">
+          <h1 className="text-center">Laura E. Collins</h1>
+          <div className="row" id="navLinks">
+            <Link to="/" type="button" className="btn btn-dark" id="navToBio">Biography</Link>
+            <Link to="/technologies" type="button" className="btn btn-dark" id="navToTech">Technologies</Link>
+            <Link to="/projects" type="button" className="btn btn-dark" id="navToProjects">Projects</Link>
+          </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default NavBar;

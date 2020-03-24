b21dfc644d0982341c236af6cf48d160777b1f10
Laura Collins <laura.e.collins1@gmail.com>
2020-03-24 18:04:34 -0500

    wip

--- src/components/shared/NavBar/NavBar.js
+++ src/components/shared/NavBar/NavBar.js
@@ -12,6 +12,7 @@ class NavBar extends React.Component {
             <Link to="/" type="button" className="btn btn-dark" id="navToBio">Biography</Link>
             <Link to="/technologies" type="button" className="btn btn-dark" id="navToTech">Technologies</Link>
             <Link to="/projects" type="button" className="btn btn-dark" id="navToProjects">Projects</Link>
+            <Link to="/contact" type="button" className="btn btn-dark" id="navToProjects">Contact</Link>
           </div>
         </nav>
       </div>

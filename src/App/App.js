69dc44d8e66075f73466e75de429574184b42e8b
Laura Collins <laura.e.collins1@gmail.com>
2020-08-28 10:47:55 -0500

    wip

--- src/App/App.js
+++ src/App/App.js
@@ -4,7 +4,10 @@ import {
   Switch,
   Route,
 } from 'react-router-dom';
-import { ThemeProvider, createTheme, Arwes } from 'arwes';
+import {
+  ThemeProvider, createTheme, Arwes, Puffs, SoundsProvider,
+  createSounds, Line, Link,
+} from 'arwes';
 import './App.scss';
 
 import NavBar from '../components/shared/NavBar/NavBar';
@@ -14,6 +17,24 @@ import Technologies from '../components/pages/Technologies/Technologies';
 import Contact from '../components/pages/Contact/Contact';
 import background from '../helpers/assets/images/background.jpg';
 import glow from '../helpers/assets/images/glow.png';
+import clickSound from '../helpers/assets/sounds/click.mp3';
+import typingSound from '../helpers/assets/sounds/typing.mp3';
+import deploySound from '../helpers/assets/sounds/deploy.mp3';
+
+const mySounds = {
+  shared: { volume: 1 },
+  players: {
+    click: {
+      sound: { src: [clickSound] },
+    },
+    typing: {
+      sound: { src: [typingSound] },
+    },
+    deploy: {
+      sound: { src: [deploySound] },
+    },
+  },
+};
 
 class App extends React.Component {
   resources = {
@@ -24,19 +45,28 @@ class App extends React.Component {
   render() {
     return (
       <ThemeProvider theme={createTheme()}>
-        <Arwes resources={this.resources}>
-        <div className="App">
-        <Router>
-        <NavBar />
-          <Switch>
-              <Route path="/" exact component={Bio} />
-              <Route path="/projects" exact component={Projects} />
-              <Route path="/technologies" exact component={Technologies} />
-              <Route path="/contact" exact component={Contact} />
-          </Switch>
-        </Router>
-        </div>
-        </Arwes>
+        <SoundsProvider sounds={createSounds(mySounds)}>
+          <Arwes resources={this.resources} animate pattern={glow} stype={{ padding: 20 }}>
+            <div className="App">
+            <Router>
+            <NavBar />
+              <Switch>
+                <Puffs>
+                  <Route path="/" exact component={Bio} />
+                  <Route path="/projects" exact component={Projects} />
+                  <Route path="/technologies" exact component={Technologies} />
+                  <Route path="/contact" exact component={Contact} />
+                </Puffs>
+              </Switch>
+            </Router>
+            <div className="footerContents d-flex row justify-content-between">
+              <Line animate />
+              <div>© Laura E. Collins</div>
+              <Link href="https://github.com/arwesjs/arwes" alt="Arwes theme">Arwes API by Romel Pérez</Link>
+            </div>
+            </div>
+          </Arwes>
+        </SoundsProvider>
       </ThemeProvider>
     );
   }
--- src/components/shared/NavBar/NavBar.js
+++ src/components/shared/NavBar/NavBar.js
@@ -1,26 +1,33 @@
 import './NavBar.scss';
 import React from 'react';
-import { Button, Header } from 'arwes';
+import {
+  Button, Header, Heading, Line,
+} from 'arwes';
 import { Link } from 'react-router-dom';
 
 class NavBar extends React.Component {
   render() {
     return (
       <div className="NavBar">
+        <Header animate>
         <nav className="container-fluid navbar">
           <div>
-            <Header animate>
-            <h1 style={{ margin: 0 }}>Laura E. Collins</h1>
-            </Header>
-            <div className="subtitle text-center">full-stack web developer</div>
+              <Heading animate>
+                <h1>Laura E. Collins</h1>
+              </Heading>
+              <Line animate />
+              <Heading animate>
+                <h5 className="subtitle text-center">full-stack web developer</h5>
+              </Heading>
           </div>
-          <div className="row wrap" id="navLinks">
-            <Link to="/" id="navToBio"><Button>Biography</Button></Link>
-            <Link to="/technologies" id="navToTech"><Button>Technologies</Button></Link>
-            <Link to="/projects" id="navToProjects"><Button>Projects</Button></Link>
-            <Link to="/contact" id="navToProjects"><Button>Contact</Button></Link>
+          <div className="row wrap mr-3" id="navLinks">
+            <Link to="/" id="navToBio"><Button animate>Biography</Button></Link>
+            <Link to="/technologies" id="navToTech"><Button animate>Technologies</Button></Link>
+            <Link to="/projects" id="navToProjects"><Button animate>Projects</Button></Link>
+            <Link to="/contact" id="navToProjects"><Button animate>Contact</Button></Link>
           </div>
         </nav>
+        </Header>
       </div>
     );
   }
